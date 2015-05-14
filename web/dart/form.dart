ac9f321190540e5407fa2ba32f78ad1c58b342f0
dragonloverlord <dragonloverlord@gmail.com>
2015-05-14 08:45:58 -0400

    I have added form.dart, deleted dialog.css, and built on to add_page.dart

---
+++ web/dart/form.dart
@@ -0,0 +1,13 @@
+library main.add_page.form;
+
+import 'dart:html';
+
+Element formDiv = null;
+
+class Form {
+  static void addForm(){
+    formDiv = new Element.tag('div');
+    formDiv.setAttribute('id', '#form-div');
+    querySelector('#overlay-div').append(formDiv);
+  }
+}
