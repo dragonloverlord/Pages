b98f0abc3d909f9d1a41c712f42bd7283e883b66
dragonloverlord <dragonloverlord@gmail.com>
2015-05-14 19:22:41 -0400

    Cleaned up and fixed null variables

--- web/dart/links.dart
+++ web/dart/links.dart
@@ -2,40 +2,34 @@ library main.links;
 
 import 'dart:html';
 
-String url = null;
-String text = null;
-String id = null;
-String containerId = null;
-String linkId = null;
-Element container = null;
-Element linkHolder = null;
-Element link = null;
+String url = 'blank';
+String text = 'blank';
+Element container = new Element.tag('div');
+Element linkHolder = new Element.tag('button');
+Element link = new Element.tag('a');
 
 
 class Links {
   static void addLink(url, text) {
-    id = 'build';
-    containerId = '#' + id;
-    linkId = containerId + '-link';
     container = new Element.tag('div');
     container.setAttribute('class', 'link-box');
-    container.setAttribute('id', id);
+    container.setAttribute('id', 'build');
     querySelector('#main-container').append(container);
     
     link = new Element.tag('a');
     link.setAttribute('class', 'link');
     link.setAttribute('href', url);
     link.setAttribute('target', '_blank');
-    link.setAttribute('id', id + '-link');
-    querySelector(containerId).append(link);
+    link.setAttribute('id', 'build-link');
+    querySelector('#build').append(link);
     
     linkHolder = new Element.tag('button');
     linkHolder.setAttribute('class', 'link-holder');
     linkHolder.setAttribute('type', 'button');
     linkHolder.appendText(text);
-    querySelector(linkId).append(linkHolder);
+    querySelector('#build-link').append(linkHolder);
     
-    querySelector('#' + id).setAttribute('id', 'complete');
-    querySelector('#' + id + '-link').setAttribute('id', 'complete-link');
+    querySelector('#build').setAttribute('id', 'complete');
+    querySelector('#build-link').setAttribute('id', 'complete-link');
   }
 }
