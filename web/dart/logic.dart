fe7909a92df0deb08e370088720e4338ca53ec7d
dragonloverlord <dragonloverlord@gmail.com>
2015-05-19 09:41:37 -0400

    Added the logic functions to the Logic class.

--- web/dart/logic.dart
+++ web/dart/logic.dart
@@ -2,9 +2,23 @@ library main.add_page.form.logic;
 
 import 'dart:html';
 
+import 'links.dart';
+
 
 class Logic {
   static void main(){
+    querySelector('#submit-button').addEventListener('click', submitButtonClicked);
+    querySelector('#cancel-button').addEventListener('click', cancelButtonClicked);
+  }
 
+  static void submitButtonClicked(mouseEvent){
+    InputElement textInputValue = querySelector('#text-input');
+    InputElement urlInputValue = querySelector('#url-input');
+
+    Links.addLink(urlInputValue.value, textInputValue.value);
+  }
+
+  static void cancelButtonClicked(mouseEvent){
+    querySelector('#overlay-div').remove();
   }
 }
